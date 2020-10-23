2663a554607dc4d945abfcc59435c8f49f47c9f1
Hlib Pylypets <pilipets.gleb@gmail.com>
2020-10-24 00:24:17 +0300

    Applied pylint recommendations to difference_module.py, similarity_module.py
    Signed-off-by: Hlib Pylypets <pilipets.gleb@gmail.com>

--- ProjectDrafts/difference_module.py
+++ ProjectDrafts/difference_module.py
@@ -1,56 +1,56 @@
 import difflib, sys, codecs
+from datetime import datetime
 from os import sep
-from os.path import isdir, join, isfile, split
+from os.path import join, isfile, split
 
 def eprint(*args, **kwargs):
-    print('ERR %s: ' % datetime.now().strftime("%d/%m/%Y %H:%M:%S"), end ='')
+    print('ERR %s: ' % datetime.now().strftime("%d/%m/%Y %H:%M:%S"), end='', file=sys.stderr)
     print(*args, file=sys.stderr, **kwargs)
     sys.exit()
 
 def difference_comparison(input_files, difference_option):
-    # https://pymotw.com/2/difflib/
-
-    compare_func = difflib.unified_diff #  a unified diff only includes modified lines and a bit of context.
+    #  a unified diff only includes modified lines and a bit of context.
+    compare_func = difflib.unified_diff
     if difference_option == 1:
         compare_func = difflib.ndiff # avoids noise
     elif difference_option == 2:
         compare_func = difflib.Differ().compare
 
-    l1, l2 = [], []
-    for n, lines in enumerate(zip(input_files[0],input_files[1])):
-        if not (n+1 % 100 == 0):
-            l1.append(lines[0])
-            l2.append(lines[1])
+    list1, list2 = [], []
+    idx = None
+    for idx, lines in enumerate(zip(input_files[0], input_files[1])):
+        if not idx+1 % 100 == 0:
+            list1.append(lines[0])
+            list2.append(lines[1])
         else:
-            diff = compare_func(l1, l2)
-            l1, l2 = '', ''
+            diff = compare_func(list1, list2)
+            list1, list2 = [], []
             print(''.join(list(diff)))
 
-    if n+1 % 100 != 0:
-        diff = compare_func(l1, l2)
+    if idx and idx+1 % 100 != 0:
+        diff = compare_func(list1, list2)
         print(''.join(list(diff)))
 
 if __name__ == '__main__':
     if len(sys.argv) < 3 or len(sys.argv) > 4:
-        eprint('Wrong amount of input arguments(%d), 3-4 expected', len(sys.argv))
+        eprint('Wrong count of input arguments(%d), 3-4 expected' % len(sys.argv))
 
-    input_files = sys.argv[-2:]
-    running_dir = split(sys.argv[0])[0]
-    for idx, f in enumerate(input_files):
+    INPUT_FILES = sys.argv[-2:]
+    RUNNING_DIR = split(sys.argv[0])[0]
+    for idx, f in enumerate(INPUT_FILES):
         if not f.startswith(sep):
-            input_files[idx] = join(running_dir, f)
-        if not isfile(input_files[idx]):
-            eprint("Given path '%s' isn't a file", input_files[idx])
+            INPUT_FILES[idx] = join(RUNNING_DIR, f)
+        if not isfile(INPUT_FILES[idx]):
+            eprint("Given path '%s' isn't a file" % INPUT_FILES[idx])
 
-    
-    argv = sys.argv[:-2]
-    difference_option = 3
-    if '--ndiff' in argv:
-        difference_option = 1
-    elif '--compare' in argv:
-        difference_option = 2
-    elif '--unified' in argv:
-        difference_option = 3
+    ARGV = sys.argv[:-2]
+    DIFFERENCE_OPTION = 3
+    if '--ndiff' in ARGV:
+        DIFFERENCE_OPTION = 1
+    elif '--compare' in ARGV:
+        DIFFERENCE_OPTION = 2
+    elif '--unified' in ARGV:
+        DIFFERENCE_OPTION = 3
 
-    input_files = [codecs.open(f, encoding='cp1252') for f in input_files]
-    difference_comparison(input_files, difference_option)
+    INPUT_FILES = [codecs.open(f, encoding='cp1252') for f in INPUT_FILES]
+    difference_comparison(INPUT_FILES, DIFFERENCE_OPTION)
--- ProjectDrafts/similarity_module.py
+++ ProjectDrafts/similarity_module.py
@@ -1,88 +1,95 @@
-import sys, string, nltk
-from os.path import isdir, join, isfile, split
+import sys, string, nltk, codecs
 from os import listdir, sep
+from os.path import isdir, join, isfile, split
 from datetime import datetime
 from sklearn.feature_extraction.text import TfidfVectorizer
 import matplotlib.pyplot as plt, numpy as np
 
 def eprint(*args, **kwargs):
-    print('ERR %s: ' % datetime.now().strftime("%d/%m/%Y %H:%M:%S"), end ='')
+    print('ERR %s: ' % datetime.now().strftime("%d/%m/%Y %H:%M:%S"), end='', file=sys.stderr)
     print(*args, file=sys.stderr, **kwargs)
     sys.exit()
 
-def coisine_similarity(file_objects, tokenizer_option):
+def cosine_similarity(file_objects, tokenizer_option):
     remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
 
     if tokenizer_option == 0: # None
-        normalize_filter = lambda text: nltk.word_tokenize(text.lower().translate(remove_punct_dict))
+        normalize_filter = lambda text: nltk.word_tokenize(
+            text.lower().translate(remove_punct_dict))
+
     elif tokenizer_option == 1: # Lemming
         lemmer = nltk.stem.WordNetLemmatizer()
-        LemTokens = lambda tokens: [lemmer.lemmatize(token) for token in tokens]
-        normalize_filter = lambda text: LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+        lem_tokens = lambda tokens: [lemmer.lemmatize(token) for token in tokens]
+        normalize_filter = lambda text: lem_tokens(
+            nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
     else: # Stemming
         stemmer = nltk.stem.porter.PorterStemmer()
-        StemTokens = lambda tokens: [stemmer.stem(token) for token in tokens]
-        normalize_filter = lambda text: StemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+        stem_tokens = lambda tokens: [stemmer.stem(token) for token in tokens]
+        normalize_filter = lambda text: stem_tokens(
+            nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
 
-    LemVectorizer = TfidfVectorizer(tokenizer=normalize_filter, stop_words='english')
-    mat_sparse = LemVectorizer.fit_transform(file_objects)
-    mat = (mat_sparse * mat_sparse.T).A
-    return mat
+    lem_vectorizer = TfidfVectorizer(tokenizer=normalize_filter, stop_words='english')
+    mat_sparse = lem_vectorizer.fit_transform(file_objects)
+    return (mat_sparse * mat_sparse.T).A
 
 def heatmap(x_labels, y_labels, values):
-    fig, ax = plt.subplots()
-    im = ax.imshow(values)
+    fig, axes = plt.subplots()
+    _ = axes.imshow(values)
 
-    ax.set_xticks(np.arange(len(x_labels)))
-    ax.set_yticks(np.arange(len(y_labels)))
+    axes.set_xticks(np.arange(len(x_labels)))
+    axes.set_yticks(np.arange(len(y_labels)))
 
-    ax.set_xticklabels(x_labels)
-    ax.set_yticklabels(y_labels)
+    axes.set_xticklabels(x_labels)
+    axes.set_yticklabels(y_labels)
 
     # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right", fontsize=10,
-         rotation_mode="anchor")
+    plt.setp(axes.get_xticklabels(), rotation=45, ha="right", fontsize=10,
+             rotation_mode="anchor")
 
     for i in range(len(y_labels)):
         for j in range(len(x_labels)):
-            text = ax.text(j, i, "%.2f"%values[i, j],
-                           ha="center", va="center", color="w", fontsize=6)
+            axes.text(j, i, "%.2f"%values[i, j],
+                      ha="center", va="center", color="w", fontsize=6)
 
     fig.tight_layout()
     plt.show()
 
 if __name__ == '__main__':
-    input_folder = sys.argv[-1]
-    if not input_folder.startswith(sep):
-        input_folder = join(split(sys.argv[0])[0], input_folder)
+    INPUT_FOLDER = sys.argv[-1]
+    if not INPUT_FOLDER.startswith(sep):
+        INPUT_FOLDER = join(split(sys.argv[0])[0], INPUT_FOLDER)
 
-    if not isdir(input_folder):
-        eprint("Given path '%s' isn't a directory", input_folder)
+    if not isdir(INPUT_FOLDER):
+        eprint("Given path '%s' isn't a directory" % INPUT_FOLDER)
 
-    file_names = [f for f in listdir(input_folder) if isfile(join(input_folder, f))]
+    FILE_NAMES = [f for f in listdir(INPUT_FOLDER) if isfile(join(INPUT_FOLDER, f))]
+    if len(FILE_NAMES) > 20:
+        eprint("Maximum amount of input files exceeded - allowed 20, received %d" % len(FILE_NAMES))
 
-    if len(file_names) > 20:
-        eprint("Maximum amount of input files exceeded - allowed 20, received %d", len(file_names))
+    OPTIONS_LIST = [x for x in sys.argv if x in ('-l, -s, --lemming, --stemming')]
+    if len(OPTIONS_LIST) > 1:
+        eprint("Incorrect working option passed, received %s" % OPTIONS_LIST)
 
-    option = [x for x in sys.argv if x in ('-l, -s, --lemming, --stemming')]
-    
-    if len(option) > 1:
-        eprint("Icorrect option passed, received %s", option)
-
-    if len(option) == 0:
-        tokenizer_option = 0 # None
-    elif option[0] in ('-l', '--lemming'):
-        tokenizer_option = 1 # lemming
+    if len(OPTIONS_LIST) == 0:
+        TOKENIZER_OPTION = 0 # None
+    elif OPTIONS_LIST[0] in ('-l', '--lemming'):
+        TOKENIZER_OPTION = 1 # lemming
     else:
-        tokenizer_option = 2 # stemming
+        TOKENIZER_OPTION = 2 # stemming
 
-    file_objects = (open(join(input_folder, f)).read() for f in file_names)
-    mat = coisine_similarity(file_objects, tokenizer_option)
+    FILE_OBJECTS = (codecs.open(join(INPUT_FOLDER, f), encoding='cp1252').read() for f in FILE_NAMES)
+    RES_MAT = cosine_similarity(FILE_OBJECTS, TOKENIZER_OPTION)
 
-    heatmap(file_names, file_names, mat)
+    if '--external' in sys.argv:
+        if len(RES_MAT) == 0:
+            print('0')
+            sys.exit()
 
-
-
-
-
-
+        output_arr = [str(len(RES_MAT[0]))]
+        for arr in RES_MAT:
+            for val in arr:
+                output_arr.append("%.2f" % val)
+        print(','.join(output_arr))
+    else:
+        heatmap(FILE_NAMES, FILE_NAMES, RES_MAT)
