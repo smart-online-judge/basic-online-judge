715bca3a9de797807cd91dfed5a8dd153f28c246
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-10-30 19:50:18 +0200

    Refactor result rendering.
    Compute the semantic and structural difference in a separate
    goroutine.
    
    Serve the results at view/ entry point.
    
    Signed-off-by: Artyom Gevorgyan <artemhevorhian@gmail.com>

--- web-service/file_upload.go
+++ web-service/file_upload.go
@@ -7,14 +7,23 @@ import (
 	"log"
 	"mime/multipart"
 	"net/http"
+	//"net/url"
 	"os"
 	"path"
+	// TODO Make sure "NLP" is seen here.
+	// I want to see where the module comes from, not guess.
+	// See how guuid is imported
 	"project/python_wrappers"
+	guuid "github.com/google/uuid"
 )
 
 var (
 	WarningLogger *log.Logger
 	ErrorLogger   *log.Logger
+	UUIDInMemCache map[guuid.UUID]bool
+	// TODO remove this, leave UUIDResult
+	UUIDIsReadyForView map[guuid.UUID]bool
+	UUIDResult map[guuid.UUID][][]float32
 )
 
 func init() {
@@ -23,11 +32,58 @@ func init() {
 		log.Fatal(err)
 	}
 
+	if _, err := os.Stat("uploaded"); os.IsNotExist(err) {
+		// TODO 0777 is too much privilege, but it does not work with 0666.
+		err := os.Mkdir("uploaded", 0777)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
 	WarningLogger = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
 	ErrorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	UUIDInMemCache = make(map[guuid.UUID]bool)
+	UUIDIsReadyForView = make(map[guuid.UUID]bool)
+	UUIDResult = make(map[guuid.UUID][][]float32)
 }
 
-func uploadFile(w http.ResponseWriter, req *http.Request) {
+func prepareViewForUUID(id guuid.UUID) {
+	// TODO think about concurrency
+	UUIDInMemCache[id] = true
+	res, err := nlp.ComputePairwiseSimilarity("uploaded", "--external")
+	if err != nil {
+		ErrorLogger.Println(err)
+	}
+	UUIDIsReadyForView[id] = true
+	UUIDResult[id] = res
+}
+
+// Handlers
+
+func viewSimilarity(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("viewSimilarity End Point hit")
+	// Retrieve view id
+	var id_ns, ok = req.URL.Query()["id"]
+	if !ok {
+		ErrorLogger.Println("Invalid url format")
+		return
+	}
+	id_str := id_ns[0]
+	id, err := guuid.Parse(id_str)
+	if err != nil {
+		ErrorLogger.Println("Invalid UUID value")
+		return
+	}
+	// If we are not ready, deny of service.
+	if _, err := UUIDIsReadyForView[id]; err == true {
+		fmt.Printf("Result for %s is not yet ready", id);
+		return
+	}
+	// Serve the result.
+	fmt.Println("%b", UUIDResult[id])
+}
+
+func uploadFiles(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("File Upload Endpoint Hit")
 	var err error
 
@@ -66,28 +122,36 @@ func uploadFile(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 
-		fmt.Printf("Uploaded File: %+v\n", fh.Filename)
-		fmt.Printf("File Size: %+v\n", fh.Size)
-		fmt.Printf("MIME Header: %+v\n", fh.Header)
-		fmt.Fprintf(w, "Successfully Uploaded File\n")
+		// fmt.Printf("Uploaded File: %+v\n", fh.Filename)
+		// fmt.Printf("File Size: %+v\n", fh.Size)
+		// fmt.Printf("MIME Header: %+v\n", fh.Header)
 	}
 
-	if res, err := nlp.ComputePairwiseSimilarity("uploaded", "--external"); err != nil {
-		ErrorLogger.Println(err)
-	} else {
-		fmt.Fprintf(w, "%v\n", res)
+	/// Report a link to the personal room
+
+	// TODO Fix concurrent access/modification
+	id := guuid.New()
+	for {
+		if _, ok := UUIDInMemCache[id]; ok {
+			id = guuid.New()
+			continue
+		}
+		break
 	}
+	go prepareViewForUUID(id)
+
+	fmt.Fprint(w, "%s", id.String())
 }
 
+// Necessary
+
 func setupRoutes() {
-	http.HandleFunc("/upload", uploadFile)
+	http.HandleFunc("/upload", uploadFiles)
+	http.HandleFunc("/view", viewSimilarity)
 	http.ListenAndServe(":8080", nil)
 }
 
 func main() {
 	fmt.Println("Hello World")
 	setupRoutes()
-
-	//res, err := nlp.ComputePairwiseSimilarity("uploaded", "--external")
-	//fmt.Println(res, err)
 }
