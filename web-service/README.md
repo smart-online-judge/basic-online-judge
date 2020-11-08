cd8d13f258b5268150bd788436c231b6a6fb2b3c
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-11-08 14:57:00 +0200

    Applied changes after running on Linux.
    Signed-off-by: Artyom Gevorgyan <artemhevorhian@gmail.com>

---
+++ web-service/README.md
@@ -0,0 +1,4 @@
+Set up your virtual environment.
+Run this in python
+>>> import nltk
+>>> nltk.download('punkt')
--- web-service/api/python_wrappers/similarity_module.py
+++ web-service/api/python_wrappers/similarity_module.py
@@ -77,8 +77,8 @@ if __name__ == '__main__':
         TOKENIZER_OPTION = 1 # lemming
     else:
         TOKENIZER_OPTION = 2 # stemming
-
-    FILE_OBJECTS = (codecs.open(join(INPUT_FOLDER, f), encoding='cp1252').read() for f in FILE_NAMES)
+        
+    FILE_OBJECTS = (codecs.open(join(INPUT_FOLDER, f), 'rb', encoding = 'utf-8').read() for f in FILE_NAMES)
     RES_MAT = cosine_similarity(FILE_OBJECTS, TOKENIZER_OPTION)
 
     if '--external' in sys.argv:
